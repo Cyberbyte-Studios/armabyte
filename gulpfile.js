f659e6a1773c44fd12b8bad8b61b5c8db629fd23
Sam Collins <theatrepro11@gmail.com>
2016-11-19 19:24:43 +0000

    Added Admin LTE styles

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ require('laravel-elixir-vue-2');
  */
 
 elixir((mix) => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+    mix.less('admin-lte/AdminLTE.less')
+        .sass('app.scss')
+        .webpack('app.js')
+        .version(['css/AdminLTE.css', 'css/app.css', 'js/app.js']);
 });
