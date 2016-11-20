4255b8cbdb8cc855ece64b79795a9aba40ecc06e
Sam Collins <theatrepro11@gmail.com>
2016-11-20 10:22:09 +0000

    Added logs as artifacts

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-        <b>Version</b> {{ shell_exec("git log -1 --pretty=format:'%h - %s' --abbrev-commit `git merge-base local-dev dev`") }}
+        <b>Version</b> 0.0.0
     </div>
     <strong>Copyright &copy; 2014-2016 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights
     reserved.
